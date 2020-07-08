fc3c98b008c380a2ba40263320214d46cfd250e9
danisz118 <daniz118@gmail.com>
2020-07-08 20:00:21 +0300

    add work methods

--- src/com/company/AddressBook.java
+++ src/com/company/AddressBook.java
@@ -1,98 +1,66 @@
 package com.company;
 
-import com.google.gson.*;
-import com.google.gson.reflect.TypeToken;
+import com.google.gson.Gson;
 
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 
 public class AddressBook {
-    public Map<Long, Record> records = new HashMap<>();
 
-    public Record addRecord(long id,String FIO, String bornDate, int numbers, String address, long timeCreateFields) throws CloneNotSupportedException {
+    private final static String FILE_NAME = "file.json";
+    private final Gson GSON = new Gson();
+    private Map<Long, Record> records = new HashMap<>();
 
-        Record record = new Record(id, FIO, bornDate, numbers, address, timeCreateFields);
-        records.put(id, record);
-        return (Record) record.clone();
+    public Record addRecord(long id, String FIO, String bornDate, int numbers, String address, long timeCreateFields)
+            throws CloneNotSupportedException {
+        records.put(id, new Record(id, FIO, bornDate, numbers, address, timeCreateFields));
+        return getRecord(id);
+    }
+
+    public Record updateRecord(long id, String FIO, String bornDate, int numbers, String address)
+            throws CloneNotSupportedException {
+        return addRecord(id, FIO, bornDate, numbers, address, 0);
     }
 
     public Record getRecord(long id) throws CloneNotSupportedException {
-        if (records.containsKey(id)) {
-            return (Record) records.get(id).clone();
-        }
-        return null;
-    }
-
-    public Record updateRecord(long id, String FIO, String bornDate, int numbers, String address, long timeCreateField) throws CloneNotSupportedException {
-        long timeCreateFields = 0;
-        Record record = new Record(id, FIO, bornDate, numbers, address, timeCreateFields);
-        records.put(id, record);
-        return getRecord(id);
+        return records.containsKey(id) ? (Record) records.get(id).clone() : null;
     }
 
     public void removeRecord(long id) {
         records.remove(id);
     }
 
-    public List<Record> allRecords() {
-        List<Record> result = new ArrayList<>();
-        for (Record record : this.records.values())
-            result.add(record);
-        return result;
+    public Collection<Record> allRecords() {
+        return records.values();
+        // если по какой-то причине нужен имено лист
+        //return records.values().stream().collect(Collectors.toList());
     }
 
-
-    public Object objectToJson(Object object) {
-        Gson gson = new Gson(); //GsonBuilder().setPrettyPrinting().create()
-        String json = gson.toJson(object);
-        return json;
+    public void store() throws IOException {
+        fileWriter(GSON.toJson(records));
     }
 
-
-    public AddressBook fromJsonToMap(String json) {
-        Gson gson = new GsonBuilder().setLenient().create();
-        return gson.fromJson(json, AddressBook.class);
-
+    public AddressBook read() throws IOException {
+        this.records = GSON.fromJson(fileReader(), Map.class);
+        return this;
     }
 
-/*
-    public void fromJsonToMap() throws IOException {
-        String json = fileReader();
-        System.out.println(json);
-        Gson gson = new Gson();
-        records = gson.fromJson(json, new TypeToken<Map<Long, Record>>() {}.getType());
-        ;
-
+    private void fileWriter(String json) throws IOException {
+        Files.write(Paths.get(FILE_NAME), json.getBytes());
     }
 
-*/
-
-/*
-public AddressBook fromJsonToMap(String json) {
-    Gson gson = new GsonBuilder().setLenient().create();
-    return gson.fromJson(json, AddressBook.class);
-
-}
-
- */
-
-    public void fileWriter(String json) throws IOException {
-        FileWriter fileWriter = new FileWriter("C:\\pbTask\\HW10\\src\\com\\company\\file.json");
-        fileWriter.write(json);
-        fileWriter.close();
-    }
-
-    public String fileReader() throws IOException {
-        FileReader fileReader = new FileReader("C:\\pbTask\\HW10\\src\\com\\company\\file.json");
-        StringBuilder stringBuilder = new StringBuilder();
-        char[] readFile = new char[1000];
-        fileReader.read(readFile); // запись с файла в буффер
-
-        for (char c : readFile)
-            stringBuilder.append(c);
-        fileReader.close();
-        return stringBuilder.toString();
+    private String fileReader() throws IOException {
+        try (Stream<String> stream = Files.lines(Paths.get(FILE_NAME), Charset.defaultCharset())) {
+            return stream.collect(Collectors.joining("\r\n"));
+        }
     }
 }
+
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,7 @@ import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -14,23 +15,31 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) throws CloneNotSupportedException, IOException {
-    AddressBook addressBook = new AddressBook();
+   /* AddressBook addressBook = new AddressBook();
 
         Date date = new Date();
 
         addressBook.addRecord(1L,"zach", "25.01.94", 063043, "ул. Ленина, 3", date.getTime());
         addressBook.addRecord(2L,"zsafda", "30.01.91", 0236, "ул. Ленина, 15", date.getTime());
         addressBook.addRecord(3L,"zasada", "25.01.45", 0145, "ул. Ленина, 63", date.getTime());
+       // addressBook.store();
+
+    */
+        AddressBook addressBook1 = new AddressBook();
+        addressBook1.read();
+        System.out.println(addressBook1.allRecords());
 
 
 
-        String json = (String) addressBook.objectToJson(addressBook);
-        addressBook.fileWriter(json);
 
 
-        String a = addressBook.fileReader();
-     //   addressBook.fromJsonToMap(a);
-        System.out.println(addressBook.allRecords());
+
+
+
+
+
+
+
 
 
 
